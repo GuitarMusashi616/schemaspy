ef7b3f85dc64b28c580635283800bfc644603442
Austin Williams <guitarmusashi@gmail.com>
2023-07-07 16:26:58 -0800

    Change routine definitions to only show comments

--- src/main/resources/layout/routines/routine.js
+++ src/main/resources/layout/routines/routine.js
@@ -18,6 +18,27 @@ $(document).ready(function() {
         .appendTo('#standard_table_wrapper .col-sm-6:eq(0)' );    	
 } );
 
+function extractSqlComments(sql) {
+    var comments = [];
+    var match;
+
+    // Match both types of comment at once
+    var regex = /(\/\*[\s\S]*?\*\/)|(--.*(?:\r?\n|$))/g;
+
+    // Capture matches
+    while ((match = regex.exec(sql)) !== null) {
+        // Remove comment markers
+        var comment = match[0];
+        
+        // Trim the line breaks
+        comment = comment.replace(/(\r?\n)+$/g, '');
+        
+        comments.push(comment);
+    }
+
+    return comments;
+}
+
 var codeElement = document.getElementById("sql-script-codemirror");
 var editor = null;
 if (null != codeElement) {
@@ -31,4 +52,8 @@ if (null != codeElement) {
 		autofocus: true,
 		readOnly: true
 	});
+
+	var codeContent = editor.getValue();
+	var comments = extractSqlComments(codeContent);
+	editor.setValue(comments.join("\n\n"));
 }
